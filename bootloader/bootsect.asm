187fe8bb88501ad41a0837ba14378b85e8da7870
Jonathan Banin <tanto.jona@gmail.com>
2018-09-20 02:00:12 +0300

    Cosmetic 2 + gdt + print32 Signed-off-by: Jonathan Banin <tanto.jona@gmail.com>
    Signed-off-by: Jonathan Banin <tanto.jona@gmail.com>

--- bootloader/bootsect.asm
+++ bootloader/bootsect.asm
@@ -1,14 +1,15 @@
 ;===============
 ;= DEFINITIONS =
 ;===============
+	BOOTSECT_SEG equ 0x0
+	STACK_SEG equ 0x9000
+	STACK_OFF equ 0x1000
+	BOOTLOADER_SEG equ 0x0
+	BOOTLOADER_OFF equ 0x7e00
+
 	[bits 16]
 	[org 0x7c00]
 
-	BOOTSECT_SEGMENT equ 0x0
-	STACK_SEGMENT equ 0x9000
-	STACK_OFFSET equ 0x1000
-	BOOTLOADER_SEGMENT equ 0x0
-	BOOTLOADER_OFFSET equ 0x7e00
 
 	%macro print_nl 1
 		mov si, %1
@@ -19,31 +20,31 @@
 ;========
 ;= CODE =
 ;========
-	mov bx, BOOTSECT_SEGMENT
+	mov bx, BOOTSECT_SEG
 	mov ds, bx
 	print_nl STARTING
 
 	print_nl SETTING_STACK
 	setup_stack:
-	mov bp, STACK_SEGMENT
+	mov bp, STACK_SEG
 	mov ss, bp
-	mov bp, STACK_OFFSET
+	mov bp, STACK_OFF
 	mov sp, bp
 
 	print_nl LOADING_BOOTLOADER
 	read:
-	mov bx, BOOTLOADER_SEGMENT
+	mov bx, BOOTLOADER_SEG
 	mov es, bx
-	mov bx, BOOTLOADER_OFFSET
+	mov bx, BOOTLOADER_OFF
 	mov dh, 50
 	call disk_load
 
 	print_nl ENTERING_BOOTLOADER
 	enter:
-	mov bx, BOOTLOADER_SEGMENT
+	mov bx, BOOTLOADER_SEG
 	mov ds, bx
 	; For entering main.asm:main
-	call BOOTLOADER_SEGMENT:main
+	call BOOTLOADER_SEG:main
 
 	%include "print.asm"
 	%include "disk.asm"
--- bootloader/main.asm
+++ bootloader/main.asm
@@ -26,57 +26,47 @@
 
 	print_nl PRINTING_REGISTERS
 	print_registers:
-		push bx
-		print R_AX
-		mov bx, ax
-		hex_down
-		print R_BX
-		pop bx
-		hex_down
-		push bx
-		print R_CX
-		mov bx, cx
-		hex_down
-		print R_DX
-		mov bx, dx
-		hex_down
-		print R_SS
-		mov bx, ss
-		hex_down
-		print R_CS
-		mov bx, cs
-		hex_down
-		pop bx
-		print R_DS
-		mov bx, ds
-		hex_down
+	push bx
+	print R_AX
+	mov bx, ax
+	hex_down
+	print R_BX
+	pop bx
+	hex_down
+	push bx
+	print R_CX
+	mov bx, cx
+	hex_down
+	print R_DX
+	mov bx, dx
+	hex_down
+	print R_SS
+	mov bx, ss
+	hex_down
+	print R_CS
+	mov bx, cs
+	hex_down
+	pop bx
+	print R_DS
+	mov bx, ds
+	hex_down
 
 	print_nl SETTING_GDT
 	setup_gdt:
-		cli
-		lgdt [gdt_descriptor]
-		mov eax, cr0
-		or eax, 0x1
-		mov cr0, eax
-		jmp CODE_SEG:PM
+	cli
+	lgdt [gdt_descriptor]
+	mov eax, cr0
+	or eax, 0x1
+	mov cr0, eax
+	jmp CODE_SEG:PM
+
 	jmp $
 	%include "gdt.asm"
 
-;========
-;= DATA =
-;========
-	SUCCESSFUL: db "Successfully entered bootloader",0
-	PRINTING_REGISTERS: db "PRINTING REGISTERS:",0
-	SETTING_GDT: db "Setting GDT",0
-	reg "AX"
-	reg "BX"
-	reg "CX"
-	reg "DX"
-	reg "DS"
-	reg "CS"
-	reg "SS"
-
-[bits 32]
+;==============
+;= CODE 32bit =
+;==============
+    [bits 32]
 	PM:
 		mov ax, DATA_SEG
 		mov ds, ax
@@ -90,4 +80,21 @@
 		call print_string_pm
 		jmp $
 		%include "print32.asm"
-		SUCCESSFUL32: db "Successful 32 mode!",0
+
+
+;========
+;= DATA =
+;========
+	SUCCESSFUL: db "Successfully entered bootloader",0
+	PRINTING_REGISTERS: db "PRINTING REGISTERS:",0
+	SETTING_GDT: db "Setting GDT",0
+	SUCCESSFUL32: db "Successful 32 mode!",0
+	reg "AX"
+	reg "BX"
+	reg "CX"
+	reg "DX"
+	reg "DS"
+	reg "CS"
+	reg "SS"
+
+
