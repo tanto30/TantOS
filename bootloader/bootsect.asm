c5567322f2ab1e47b6b5937625f306733e36a100
Jonathan Banin <tanto.jona@gmail.com>
2018-09-20 00:17:36 +0300

    Cosmetic 1 Signed-off-by: Jonathan Banin <tanto.jona@gmail.com>
    Signed-off-by: Jonathan Banin <tanto.jona@gmail.com>

--- bootloader/bootsect.asm
+++ bootloader/bootsect.asm
@@ -1,58 +1,61 @@
-;DEFINITIONS:
-    [bits 16]
-    [org 0x7c00]
-    BOOTSECT_SEGMENT equ 0x0
+;===============
+;= DEFINITIONS =
+;===============
+	[bits 16]
+	[org 0x7c00]
 
-    STACK_SEGMENT equ 0x9000
-    STACK_OFFSET equ 0x1000
+	BOOTSECT_SEGMENT equ 0x0
+	STACK_SEGMENT equ 0x9000
+	STACK_OFFSET equ 0x1000
+	BOOTLOADER_SEGMENT equ 0x0
+	BOOTLOADER_OFFSET equ 0x7e00
 
-    BOOTLOADER_SEGMENT equ 0x0
-    BOOTLOADER_OFFSET equ 0x7e00
+	%macro print_nl 1
+		mov si, %1
+		call print_
+		call line_down_
+	%endmacro
 
-    %macro print_nl 1
-        mov si, %1
-        call print_
-        call line_down_
-    %endmacro
+;========
+;= CODE =
+;========
+	mov bx, BOOTSECT_SEGMENT
+	mov ds, bx
+	print_nl STARTING
 
-;CODE:
-    mov bx, BOOTSECT_SEGMENT
-    mov ds, bx
-    print_nl STARTING
+	print_nl SETTING_STACK
+	setup_stack:
+	mov bp, STACK_SEGMENT
+	mov ss, bp
+	mov bp, STACK_OFFSET
+	mov sp, bp
 
-    print_nl SETTING_STACK
-    setup_stack:
-    mov bp, STACK_SEGMENT
-    mov ss, bp
-    mov bp, STACK_OFFSET
-    mov sp, bp
+	print_nl LOADING_BOOTLOADER
+	read:
+	mov bx, BOOTLOADER_SEGMENT
+	mov es, bx
+	mov bx, BOOTLOADER_OFFSET
+	mov dh, 50
+	call disk_load
 
-    print_nl LOADING_BOOTLOADER
-    read:
-    mov bx, BOOTLOADER_SEGMENT
-    mov es, bx
-    mov bx, BOOTLOADER_OFFSET
-    mov dh, 50
-    call disk_load
+	print_nl ENTERING_BOOTLOADER
+	enter:
+	mov bx, BOOTLOADER_SEGMENT
+	mov ds, bx
+	; For entering main.asm:main
+	call BOOTLOADER_SEGMENT:main
 
-    print_nl ENTERING_BOOTLOADER
-    enter:
-    mov bx, BOOTLOADER_SEGMENT
-    mov ds, bx
-    ; For entering main.asm:main
-    call BOOTLOADER_SEGMENT:main
-    ; For entering kernel.c
-    ; jmp BOOTLOADER_SEGMENT:BOOTLOADER_OFFSET
+	%include "print.asm"
+	%include "disk.asm"
 
-    %include "print.asm"
-    %include "disk.asm"
+;========
+;= DATA =
+;========
+	STARTING: db "Starting",0
+	SETTING_STACK: db "Setting up stack | 91000h",0
+	LOADING_BOOTLOADER: db "Loading Bootloader | 10000h",0
+	ENTERING_BOOTLOADER: db "Entering Bootloader| jmp 1000h:0",0
+	times 510-($-$$) db 0
+	dw 0xaa55
 
-;DATA:
-    STARTING: db "Starting",0
-    SETTING_STACK: db "Setting up stack | 91000h",0
-    LOADING_BOOTLOADER: db "Loading Bootloader | 10000h",0
-    ENTERING_BOOTLOADER: db "Entering Bootloader| jmp 1000h:0",0
-    times 510-($-$$) db 0
-    dw 0xaa55
-
-    %include "main.asm"
+	%include "main.asm"
--- bootloader/main.asm
+++ bootloader/main.asm
@@ -1,85 +1,93 @@
-;DEFINITIONS:
+;===============
+;= DEFINITIONS =
+;===============
    %macro print 1
-        mov si, %1
-        call print_
+		mov si, %1
+		call print_
    %endmacro
    %macro hex_down 0
-        call print_hex
-        call line_down_
+		call print_hex
+		call line_down_
    %endmacro
    %macro reg 1
-        %strcat label "R_", %1, ": "
-        %deftok label label
-        %strcat value %1, ": "
-        label db value,0
+		%strcat label "R_", %1, ": "
+		%deftok label label
+		%strcat value %1, ": "
+		label db value,0
    %endmacro
    %include "utils.asm"
-;CODE:
-    main:
-    call line_down_
-    print_nl SUCCESSFUL
 
-    print_nl PRINTING_REGISTERS
-    print_registers:
-        push bx
-        print R_AX
-        mov bx, ax
-        hex_down
-        print R_BX
-        pop bx
-        hex_down
-        push bx
-        print R_CX
-        mov bx, cx
-        hex_down
-        print R_DX
-        mov bx, dx
-        hex_down
-        print R_SS
-        mov bx, ss
-        hex_down
-        print R_CS
-        mov bx, cs
-        hex_down
-        pop bx
-        print R_DS
-        mov bx, ds
-        hex_down
+;========
+;= CODE =
+;========
+	main:
+	call line_down_
+	print_nl SUCCESSFUL
 
-    print_nl SETTING_GDT
-    setup_gdt:
-        cli
-        lgdt [gdt_descriptor]
-        mov eax, cr0
-        or eax, 0x1
-        mov cr0, eax
-        jmp CODE_SEG:PM
-    jmp $
-    %include "gdt.asm"
+	print_nl PRINTING_REGISTERS
+	print_registers:
+		push bx
+		print R_AX
+		mov bx, ax
+		hex_down
+		print R_BX
+		pop bx
+		hex_down
+		push bx
+		print R_CX
+		mov bx, cx
+		hex_down
+		print R_DX
+		mov bx, dx
+		hex_down
+		print R_SS
+		mov bx, ss
+		hex_down
+		print R_CS
+		mov bx, cs
+		hex_down
+		pop bx
+		print R_DS
+		mov bx, ds
+		hex_down
+
+	print_nl SETTING_GDT
+	setup_gdt:
+		cli
+		lgdt [gdt_descriptor]
+		mov eax, cr0
+		or eax, 0x1
+		mov cr0, eax
+		jmp CODE_SEG:PM
+	jmp $
+	%include "gdt.asm"
+
+;========
+;= DATA =
+;========
+	SUCCESSFUL: db "Successfully entered bootloader",0
+	PRINTING_REGISTERS: db "PRINTING REGISTERS:",0
+	SETTING_GDT: db "Setting GDT",0
+	reg "AX"
+	reg "BX"
+	reg "CX"
+	reg "DX"
+	reg "DS"
+	reg "CS"
+	reg "SS"
+
 [bits 32]
-    PM:
-        mov ax, DATA_SEG
-        mov ds, ax
-        mov ss, ax
-        mov es, ax
-        mov gs, ax
-        mov fs, ax
-        mov ebp, 0x90000
-        mov esp, ebp
-        mov ebx, SUCCESSFUL32
-        call print_string_pm
-        jmp $
-        %include "print32.asm"
-        SUCCESSFUL32: db "Successful 32 mode!",0
-[bits 16]
-;DATA:
-    SUCCESSFUL: db "Successfully entered bootloader",0
-    PRINTING_REGISTERS: db "PRINTING REGISTERS:",0
-    SETTING_GDT: db "Setting GDT",0
-    reg "AX"
-    reg "BX"
-    reg "CX"
-    reg "DX"
-    reg "DS"
-    reg "CS"
-    reg "SS"
+	PM:
+		mov ax, DATA_SEG
+		mov ds, ax
+		mov ss, ax
+		mov es, ax
+		mov gs, ax
+		mov fs, ax
+		mov ebp, 0x90000
+		mov esp, ebp
+		mov ebx, SUCCESSFUL32
+		call print_string_pm
+		jmp $
+		%include "print32.asm"
+		SUCCESSFUL32: db "Successful 32 mode!",0
